fa301b28e30ade334202e69255d55f752b6d50cb
Sarah Murphy <sarahelizabeth.murphy@keyin.com>
2025-02-10 17:18:58 -0330

    Added test drivers for height, width and area

--- TestMyRectangle.java
+++ TestMyRectangle.java
@@ -9,14 +9,24 @@ public class TestMyRectangle {
         
         MyRectangle rectangleTwo = new MyRectangle(1, 5, 3, 7);
 
-        // Testing methods
+        // Testing class methods
         System.out.println("Rectangle One: " + rectangleOne);
+        System.out.println("Height of Rectangle One: " + rectangleOne.getHeight());
+        System.out.println("Width of Rectangle One: " + rectangleOne.getWidth());
+        System.out.println("Area of Rectangle One: " + rectangleOne.getArea());
+        System.out.println("");
         System.out.println("Rectangle Two: " + rectangleTwo);
+        System.out.println("Height of Rectangle Two: " + rectangleTwo.getHeight());
+        System.out.println("Width of Rectangle Two: " + rectangleTwo.getWidth());
+        System.out.println("Area of Rectangle Two: " + rectangleTwo.getArea());
 
         // Updating instances with new values
         rectangleOne.setTopLeft(new MyPoint(3, 6));
         rectangleOne.setBottomRight(new MyPoint(8, 4));
-
+        System.out.println("");
         System.out.println("Updated points for Rectangle One: " + rectangleOne);
+        System.out.println("Updated Height of Rectangle One: " + rectangleOne.getHeight());
+        System.out.println("Updated Width of Rectangle One: " + rectangleOne.getWidth());
+        System.out.println("Updated Area of Rectangle One: " + rectangleOne.getArea());
     }
 }
